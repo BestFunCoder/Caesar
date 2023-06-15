cf6f6c5bb762fa1e3d5662afb094872059c4ae54
OlegGutsol <oleggutsol80@gmail.com>
2023-06-15 23:33:41 +0300

    Add method main

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Greeting.greeting();
+        int operationSelect = Operation.operationSelection();
+        int  choosingEncryptionMethod  = EncryptionMethod.selectEncryptionMethod();
+
+            }
+}
