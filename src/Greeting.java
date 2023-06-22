bffa15037c409d6921a817344a60659d6c0cfc77
BestFunCoder <oleggutsol80@gmail.com>
2023-06-22 13:53:24 +0300

    Optimized class text

--- src/Greeting.java
+++ src/Greeting.java
@@ -1,6 +1,7 @@
 public  class Greeting {
-
-    public static void greeting(){
-        System.out.println("Вітаю в програмі-криптоаналізаторі \"Caesar\" !");
+public static final String TEXT_GREETING = "Вітаю в програмі-криптоаналізаторі \"Caesar\"!\n" +
+        "**********";
+    public void greeting(){
+        System.out.println(TEXT_GREETING);
     }
 }
