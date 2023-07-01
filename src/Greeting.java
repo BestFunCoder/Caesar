3c02474da516ad89ad04b8c5ce9ab2eb42b05789
BestFunCoder <oleggutsol80@gmail.com>
2023-07-01 22:16:47 +0300

    Fix class

--- src/Greeting.java
+++ src/Greeting.java
@@ -1,7 +1,5 @@
 public  class Greeting {
-public static final String TEXT_GREETING = """
-        Вітаю в програмі-криптоаналізаторі \"Caesar\"!
-        **********""";
+public static final String TEXT_GREETING = "=== Вітаю в програмі-криптоаналізаторі \"Caesar\"! ===";
     public void greeting(){
         System.out.println(TEXT_GREETING);
     }
