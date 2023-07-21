56b203503533292b65ca2bf8674b849a4e3d3cc4
BestFunCoder <136710923+BestFunCoder@users.noreply.github.com>
2023-07-21 18:37:35 +0300

    Update ChoiceEncryptionMethod.java
    test

--- src/ChoiceEncryptionMethod.java
+++ src/ChoiceEncryptionMethod.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class ChoiceEncryptionMethod extends ChoiceOfOperation {
+public class ChoiceEncryptionMethod extends ChoiceOfOperation {String s = "для пул реквесту";
     private final String MENU_ITEMS = """
             1 - Метод з вибором ключа.
             2 - Метод Brute Force.          
