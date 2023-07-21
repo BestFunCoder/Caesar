9badfabe526a2159b95617137985ee18d64d1ea0
BestFunCoder <136710923+BestFunCoder@users.noreply.github.com>
2023-07-21 18:43:52 +0300

    Update Main.java
    test

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-
+String s = "для пул реквесту";
        //привітання
         new Greeting().greeting();
         //запуск програми
